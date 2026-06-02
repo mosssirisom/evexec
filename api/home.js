f6b7f62668ad520e7ee9f1b68dfa2a69e2688867
mosssirisom <nitisatsirisom@hotmail.co.uk>
2026-06-02 19:24:03 +0100

    Fix homepage serverless file path crash

--- api/home.js
+++ api/home.js
@@ -1,17 +1,25 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 module.exports = function handler(req, res) {
-  let html = fs.readFileSync('index.html', 'utf8');
-  const tag = '<script src="./public/js/google-places-autocomplete.js"></script>';
+  try {
+    const filePath = path.join(process.cwd(), 'index.html');
+    let html = fs.readFileSync(filePath, 'utf8');
 
-  if (!html.includes('google-places-autocomplete.js')) {
-    html = html.replace('</body>', tag + '\n</body>');
+    const tag = '<script src="/public/js/google-places-autocomplete.js"></script>';
+
+    if (!html.includes('google-places-autocomplete.js')) {
+      html = html.replace('</body>', tag + '\n</body>');
+    }
+
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('Cache-Control', 'no-store, max-age=0');
+    res.statusCode = 200;
+    res.end(html);
+  } catch (error) {
+    res.statusCode = 500;
+    res.end('Homepage failed to load');
   }
-
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.setHeader('Cache-Control', 'no-store, max-age=0');
-  res.statusCode = 200;
-  res.end(html);
 };
