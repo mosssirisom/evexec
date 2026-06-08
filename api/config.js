a9c8aa8f777931e119a265dbe085bed6eba5cccc
mosssirisom <nitisatsirisom@hotmail.co.uk>
2026-06-08 21:04:32 +0100

    Expose Google Maps browser key in config

--- api/config.js
+++ api/config.js
@@ -8,6 +8,7 @@ module.exports = function handler(req, res) {
   res.end(JSON.stringify({
     supabaseUrl: process.env.SUPABASE_URL || 'https://yoltkmhtxwluqxxpewbl.supabase.co',
     supabaseAnon: process.env.SUPABASE_ANON_KEY || '',
-    vapidPublic: process.env.VAPID_PUBLIC_KEY || ''
+    vapidPublic: process.env.VAPID_PUBLIC_KEY || '',
+    googleMapsKey: process.env.GOOGLE_MAPS_BROWSER_KEY || process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || process.env.GOOGLE_MAPS_API_KEY || ''
   }));
 };
