4eadb92121cd03116cc1fc08dd56539ed9b4c448
mosssirisom <nitisatsirisom@hotmail.co.uk>
2026-06-08 21:33:17 +0100

    Use GOOGLE_MAPS_API_KEY for booking autocomplete

--- api/config.js
+++ api/config.js
@@ -9,6 +9,6 @@ module.exports = function handler(req, res) {
     supabaseUrl: process.env.SUPABASE_URL || 'https://yoltkmhtxwluqxxpewbl.supabase.co',
     supabaseAnon: process.env.SUPABASE_ANON_KEY || '',
     vapidPublic: process.env.VAPID_PUBLIC_KEY || '',
-    googleMapsKey: process.env.GOOGLE_MAPS_BROWSER_KEY || process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || process.env.GOOGLE_MAPS_API_KEY || ''
+    googleMapsKey: process.env.GOOGLE_MAPS_API_KEY || process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''
   }));
 };
